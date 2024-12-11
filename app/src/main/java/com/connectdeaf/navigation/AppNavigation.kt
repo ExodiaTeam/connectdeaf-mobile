badd46ba257f18bff7a8093c658cb09140936926
Elenildo Filho <elenildovas123@gmail.com>
2024-12-11 19:33:07 -0300

    feat: definindo a navegação das telas

--- app/src/main/java/com/connectdeaf/navigation/AppNavigation.kt
+++ app/src/main/java/com/connectdeaf/navigation/AppNavigation.kt
@@ -1,2 +1,26 @@
 package com.connectdeaf.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.connectdeaf.ui.screens.HomeScreen
+import com.connectdeaf.ui.screens.ProfileScreen
+import com.connectdeaf.ui.screens.RegisterScreenPreview
+import com.connectdeaf.ui.screens.ServicesScreen
+
+
+@Composable
+fun AppNavigation(navController: NavHostController, modifier: Modifier = Modifier) {
+    NavHost(
+        navController = navController,
+        startDestination = "register",
+        modifier = modifier
+    ) {
+        composable("home") { HomeScreen() }
+        composable("register") { RegisterScreenPreview() }
+        composable("services") { ServicesScreen() }
+        composable("profile") { ProfileScreen() }
+    }
+}
