e7a375b87ee2bc4682e62d1ca90d6ef308343e69
Vitor <vitorloula3@gmail.com>
2025-01-26 23:40:42 -0300

    feat(componentes): Adiciona componentes relacionados a serviços

--- app/src/main/java/com/connectdeaf/ui/components/ServiceCard.kt
+++ app/src/main/java/com/connectdeaf/ui/components/ServiceCard.kt
@@ -1,11 +1,17 @@
 package com.connectdeaf.ui.components
+
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
@@ -14,24 +20,30 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.connectdeaf.R
+import com.connectdeaf.ui.theme.ErrorColor
+import com.connectdeaf.ui.theme.PrimaryColor
 
 @Composable
 fun ServiceCard(
-    id: Int,
+    id: String,
+    name: String, // Nome do serviço
     description: String,
     image: String? = null,
     value: Double,
-    onClick: (id: String) -> Unit
+    onClick: (id: String) -> Unit,
+    isProfessional: Boolean,
+    onDeleteClick: (id: String) -> Unit = {},
+    onEditClick: (id: String) -> Unit = {}
 ) {
-
     Card(
         modifier = Modifier
-            .clickable { onClick(id.toString()) }
+            .clickable { onClick(id) }
             .fillMaxWidth()
             .widthIn(max = 180.dp)
             .background(Color.White)
@@ -40,8 +52,10 @@ fun ServiceCard(
         colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.background),
         elevation = CardDefaults.cardElevation(4.dp)
     ) {
-        Column {
-            // Image or Placeholder
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -54,11 +68,11 @@ fun ServiceCard(
                             .background(Color.Black),
                         contentAlignment = Alignment.Center
                     ) {
-
+                        // Placeholder: Pode adicionar um ícone ou texto de placeholder aqui
                     }
                 } else {
                     Image(
-                        painter = painterResource( id = R.drawable.doutor),
+                        painter = painterResource(id = R.drawable.doutor), // Usando uma imagem padrão
                         contentDescription = "Service Image",
                         contentScale = ContentScale.Crop,
                         modifier = Modifier.fillMaxSize()
@@ -66,12 +80,27 @@ fun ServiceCard(
                 }
             }
 
+            // Nome do serviço
+            Column(
+                modifier = Modifier
+                    .padding(horizontal = 8.dp, vertical = 4.dp)
+                    .fillMaxWidth()
+            ) {
+                Text(
+                    text = name,
+                    fontSize = 16.sp,
+                    color = Color.Black,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+
             // Value and Description
             Column(
                 modifier = Modifier
                     .padding(horizontal = 8.dp, vertical = 4.dp)
+                    .fillMaxWidth()
             ) {
-                Text(text = "A partir de $value", fontSize = 14.sp, color = Color.Black)
+                Text(text = "A partir de R$ $value", fontSize = 14.sp, color = Color.Black)
                 Text(
                     text = description,
                     fontSize = 12.sp,
@@ -80,19 +109,48 @@ fun ServiceCard(
                     overflow = TextOverflow.Ellipsis
                 )
             }
+
+            if (isProfessional) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 15.dp),
+                    horizontalArrangement = Arrangement.End
+                ) {
+                    IconButton(onClick = { onEditClick(id) }) {
+                        Icon(
+                            imageVector = Icons.Filled.Edit,
+                            contentDescription = "Editar",
+                            tint = PrimaryColor,
+
+                        )
+                    }
+                    IconButton(onClick = { onDeleteClick(id) }) {
+                        Icon(
+                            imageVector = Icons.Filled.Delete,
+                            contentDescription = "Deletar",
+                            tint = ErrorColor,
+                            modifier = Modifier.size(50.dp)
+                        )
+                    }
+                }
+            }
         }
     }
 }
-
 
 @Preview
 @Composable
 private fun ServiceCardPreview() {
     ServiceCard(
-        id = 1,
+        id = "1",
+        name = "Serviço de Limpeza",
         description = "Oferecemos serviços de limpeza residencial e comercial com alta qualidade e preços competitivos.",
         image = null,
         value = 150.00,
-        onClick = { id -> println("Clicked on service with id: $id") }
+        onClick = { id -> println("Clicked on service with id: $id") },
+        isProfessional = true,
+        onDeleteClick = { id -> println("Delete clicked on service with id: $id") },
+        onEditClick = { id -> println("Edit clicked on service with id: $id") }
     )
 }
